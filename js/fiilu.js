60d58035ad11f2ccc3842071f23dd38612ce8805
seeeya <nikoatte95@gmail.com>
2017-11-08 18:15:29 +0200

    added some elements for food elements

--- js/fiilu.js
+++ js/fiilu.js
@@ -9,7 +9,7 @@ function getFood() {
 	jQuery(function($) {
 		  $(".food-fiilu").rss("https://www.fazer.fi/api/location/menurss/current?pageId=29801&language=fi",
 		  {
-			entryTemplate:'<div class="days"><h1>{title}</h1><br/>{body} </div>'
+			entryTemplate:'<div class="days"><h1>{title}</h1><br/><div class="food">{body}</div></div>'
 		  })
 	});
 }
