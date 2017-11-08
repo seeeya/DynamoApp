3fabf18c7658b091821b5c9365a5f701dcff36eb
seeeya <nikoatte95@gmail.com>
2017-11-08 18:54:19 +0200

    added some things :P

--- js/fiilu.js
+++ js/fiilu.js
@@ -19,4 +19,13 @@ function orderList() {
 	$("br").remove();
 	$(".food-fiilu").prepend($(".days table"));
 	$(".days table").remove();
+	
+	$("h2").insertAfter("<div class='foodList'>");
+	$('.food-fiilu div h2').each(function(){ 
+    var $set = $(this).nextUntil("h2").andSelf();
+    $set.wrapAll('<div class="day' + $(this).html() +'" />');
+});
+	
+	
+	
 }
