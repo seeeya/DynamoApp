7d416e8cfe4e50b487eedbae5e6c15c35df294be
seeeya <nikoatte95@gmail.com>
2017-11-08 18:00:20 +0200

    added some js

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8" />
-<title>DynamoApp TBD</title>
+<title>LutakkoApp TBD</title>
  
  <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108664137-1"></script>
@@ -13,9 +13,17 @@
 
   gtag('config', 'UA-108664137-1');
 </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="/js/jquery.rss.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min.js"></script>
+<script src="/js/fiilu.js"></script>
 </head>
 <body id="index" class="home">
+
 <?php
+
+/*
 $dir    = './';
 $files1 = scandir($dir);
 
@@ -27,11 +35,12 @@ foreach ($files1 as $value) {
 			 echo "<a href='$value'>$value</a> <br />\n";
 		}
 		}
-		
 	}
-   
-}
-
+}*/
 ?>
+
+<div class="content">
+<div class="food-fiilu"></div>
+</div>
 </body>
 </html>
---
+++ js/fiilu.js
@@ -0,0 +1,15 @@
+//Scripts By N. V.
+
+$(document).ready(function() {
+	
+	getFood();
+});
+
+function getFood() {
+	jQuery(function($) {
+		  $(".food-fiilu").rss("https://www.fazer.fi/api/location/menurss/current?pageId=29801&language=fi",
+		  {
+			entryTemplate:'<div class="days"><h1>{title}</h1><br/>{body} </div>'
+		  })
+	});
+}
