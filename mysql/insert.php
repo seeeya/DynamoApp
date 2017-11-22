3599ca33a22a9d46a2139bd3611ff3e3e1817f44
phakala <phakala@live.fi>
2017-11-23 00:26:07 +0200

    testing

--- mysql/insert.php
+++ mysql/insert.php
@@ -4,7 +4,7 @@ $place = $_GET["place"];
 $time = $_GET["time"];
 $id = $_GET["id"];
 $query = "INSERT INTO visit (place, enter_time, visitor) VALUES ('". $place . "', '" . $time . "', '" . $id . "')";
-if($conn->query($query) == TRUE) {
+if($conn->query($query) === TRUE) {
 	echo "Insert successful";
 }
 else echo "Error " . $conn->error;
