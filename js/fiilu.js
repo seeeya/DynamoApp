ce89aa3d16d2ddc1ed8e7649d821c41cdeb0425a
seeeya <nikoatte95@gmail.com>
2017-11-22 21:18:51 +0200

    table scaling.. for fiilu

--- js/fiilu.js
+++ js/fiilu.js
@@ -58,6 +58,15 @@ $set.wrapAll('<div class="day ' + $(this).html().replace(/&nbsp;/gi,'') +'" />')
 });
 
 	$("button").each(function() { $(this).html($(this).html().replace(/&nbsp;/gi,''));});
-	
+	var HeightDiv = $("div").height();
+    var HeightTable = $("table").height();
+    if (HeightTable > HeightDiv) {
+        var FontSizeTable = parseInt($("table").css("font-size"), 10);
+        while (HeightTable > HeightDiv && FontSizeTable > 5) {
+            FontSizeTable--;
+            $("table").css("font-size", FontSizeTable);
+            HeightTable = $("table").height();
+        }
+    }
 }
 
