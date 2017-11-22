d7a8ce41060f83fcfcd3517957e894941dae3b2c
phakala <phakala@live.fi>
2017-11-23 00:11:57 +0200

    asd

--- mysql/test.php
+++ mysql/test.php
@@ -1,3 +1,7 @@
 <?php
-echo "HALOO";
+$place = $_GET("place");
+$time = $_GET("time");
+$id = $_GET("id");
+$query = "INSERT INTO visit (place, enter_time, visitor) VALUES ('". $place . "', '" . $time . "', '" . $id . "')";
+echo $query;
 ?>
