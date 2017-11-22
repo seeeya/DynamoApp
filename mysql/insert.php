dbdd533951ed006c1a7c68b76fc2375ebeca781a
phakala <phakala@live.fi>
2017-11-23 00:29:46 +0200

    last test

--- mysql/insert.php
+++ mysql/insert.php
@@ -1,13 +1,23 @@
 <?php
-require "mysql_conn.php";
+$dbname = "dynamoapp";
+$dbuser = "dynamoapp";
+$dbpassword = "yFgyeJicK59a3bEq";
+$server = "walkonen.fi";
+$conn = mysqli_connect($server, $dbuser, $dbpassword, $dbname);
 $place = $_GET["place"];
 $time = $_GET["time"];
 $id = $_GET["id"];
 $query = "INSERT INTO visit (place, enter_time, visitor) VALUES ('". $place . "', '" . $time . "', '" . $id . "')";
-if($conn->query($query) === TRUE) {
-	echo "Insert successful";
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-else echo "Error " . $conn->error;
-conn->close();
+if (mysqli_query($conn, $sql)) {
+    echo "Success";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
 
 ?>
