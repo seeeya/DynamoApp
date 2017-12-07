6425c5ff97b25e4e77258b69554337ab7bf2252b
phakala <phakala@live.fi>
2017-12-07 14:43:33 +0200

    beacon landing page

---
+++ download/index.php
@@ -0,0 +1,14 @@
+<?php
+$place = $_GET["place"];
+if ($place == "fiilu") {
+	$restaurant = "Ravintola Fiilu";
+}
+else if ($place == "sodexo") {
+	$restaurant = "JAMK Ravintola Bittipannu";
+}
+?>
+<h1>View menus and reviews in LutakkoApp!</h1>
+<p>You are near <?php echo $restaurant ?>, want to check it out?
+	<br>Open with the app, or download the app here!</p>
+<a href="walkonen.fi/apps/dynamoapp/download/redirect.html?place=<?php echo $place?>">Lets go!</a>
+<p id="below">The button will prompt you to open the Lutakko App, or if not installed will redirect you to the Play Store</p>
