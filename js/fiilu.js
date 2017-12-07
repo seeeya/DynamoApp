fc2f360ee726926c5ccdbfdbf691f64242ac0ee7
seeeya <nikoatte95@gmail.com>
2017-12-07 18:08:40 +0200

    sodexo fix

--- js/fiilu.js
+++ js/fiilu.js
@@ -44,13 +44,17 @@ $( "h1" ).click(function() {
 $( "button" ).click(function() {$(".day").hide();	$(this).parent(".buttons").parent(".days").children(".food").children(".day."+$(this).html()).slideToggle(300);});
 
 function getFood() {
+	if($(".food-fiilu").length) {
 	jQuery(function($) {
 		  $(".food-fiilu").rss("https://www.fazer.fi/api/location/menurss/current?pageId=29801&language=fi",
 		  {
 			entryTemplate:'<div class="days"><h1>{title}</h1><br/><div class="food">{body}</div></div>'
 		  })
 	});
+	}
+	if($(".sodexo-today").length) {
 	getSodexo();
+	}
 }
 function orderList() {
 	var buttons = false;
