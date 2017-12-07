2657a8a3f30655a2a3a5f09f4565cdf60e7debc8
seeeya <nikoatte95@gmail.com>
2017-12-07 19:27:57 +0200

    cfazs

--- pages/frontpage.php
+++ pages/frontpage.php
@@ -1,1 +1,8 @@
+<?php 
+require_once("functions.php");
+$page = new sitesData();
+?>
+
 <h2>Welcome to LutakkoApp</h2> 
+<h4>You can find content from here:</h4>
+<?=$page->listMenu();?>
--- pages/sodexo.php
+++ pages/sodexo.php
@@ -1,4 +1,6 @@
-<h2>JAMK Restaurant Bittipannu / Dynamo </h2>
+
+<div class="page-container"><h2>JAMK Restaurant Bittipannu / Dynamo </h2>
 <p> Todays menu: </p>
 <div class="sodexo-today"></div>
+</div>
 
