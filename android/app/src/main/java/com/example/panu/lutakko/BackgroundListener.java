93f009ae5f8b93cc71af73f9f689cc1bbce8a541
phakala <phakala@live.fi>
2017-12-08 17:51:41 +0200

    fixed db saving

--- android/app/src/main/java/com/example/panu/lutakko/BackgroundListener.java
+++ android/app/src/main/java/com/example/panu/lutakko/BackgroundListener.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
@@ -16,13 +17,33 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLEncoder;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import io.proximi.proximiiolibrary.ProximiioAPI;
 import io.proximi.proximiiolibrary.ProximiioGeofence;
@@ -43,8 +64,18 @@ public class BackgroundListener extends BroadcastReceiver {
         }
         switch (intent.getAction()) {
             case ProximiioAPI.ACTION_GEOFENCE_ENTER:
-                String pageurl = "http://walkonen.fi/apps/dynamoapp/";
                 geofence = intent.getParcelableExtra(ProximiioAPI.EXTRA_GEOFENCE);
+                String pageurl = "https://walkonen.fi/apps/dynamoapp/";
+                switch (geofence.getName()) {
+                    case "Ravintola Fiilu":
+                        pageurl = "https://walkonen.fi/apps/dynamoapp/?page=fiilu";
+                        intent.putExtra("URL", pageurl);
+                        break;
+                    case "JAMK Ravintola Bittipannu":
+                        pageurl = "https://walkonen.fi/apps/dynamoapp/?page=sodexo";
+                        intent.putExtra("URL", pageurl);
+                        break;
+                }
                 Intent resultIntent = new Intent(context, WebViewActivity.class);
                 resultIntent.putExtra("URL", pageurl);
                 PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
@@ -58,7 +89,6 @@ public class BackgroundListener extends BroadcastReceiver {
                 mBuilder.setContentIntent(resultPendingIntent);
                 notifyManager.notify(1, mBuilder.build());
                 Date dNow = new Date();
-                ft = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
                 String time = ft.format(dNow).toString();
                 String url_name = null;
                 try {
@@ -67,7 +97,7 @@ public class BackgroundListener extends BroadcastReceiver {
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
-                String url = "http://walkonen.fi/apps/dynamoapp/mysql/insert.php?place="+url_name+"&time="+time+"&id="+phoneid+"";
+                String url = "https://walkonen.fi/apps/dynamoapp/mysql/insert.php?place="+url_name+"&time="+time+"&id="+phoneid+"";
                 RequestQueue queue = Volley.newRequestQueue(context);
                 StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                         new Response.Listener<String>() {
@@ -113,7 +143,7 @@ public class BackgroundListener extends BroadcastReceiver {
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
-                String u_url = "http://walkonen.fi/apps/dynamoapp/mysql/update.php?place="+url_name1+"&id="+phoneid+"&duration="+dwellint+"&exit="+exittime+"";
+                String u_url = "https://walkonen.fi/apps/dynamoapp/mysql/update.php?place="+url_name1+"&id="+phoneid+"&duration="+dwellint+"&exit="+exittime+"";
                 RequestQueue queue1 = Volley.newRequestQueue(context);
                 StringRequest stringRequest1 = new StringRequest(Request.Method.GET, u_url,
                         new Response.Listener<String>() {
