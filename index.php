d4d1c4374383a42af17d3554d3b8c053faafa483
phakala <phakala@live.fi>
2017-12-11 20:49:20 +0200

    review scores on pages

--- index.php
+++ index.php
@@ -32,6 +32,7 @@ $page = new sitesData();
 <script src="./js/fiilu.js"></script>
 <script src="./js/tether.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 </head>
 <body id="index" class="home">
--- pages/rajis.php
+++ pages/rajis.php
@@ -1,5 +1,8 @@
 
 <div class="page-container"><h2>JAMK Restaurant Radis / Pääkampus </h2>
+<?php require_once("mysql/average.php");
+getAverage();
+?>
 <p> Todays menu: </p>
 <div class="rajis-today"></div>
 </div>
