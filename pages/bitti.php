fa3f4d3291a89132806b0e7a7dbdb41a814774d7
phakala <phakala@live.fi>
2017-12-11 21:01:03 +0200

    averages on other pages

--- pages/bitti.php
+++ pages/bitti.php
@@ -1,5 +1,8 @@
 
 <div class="page-container"><h2>JAMK Restaurant Bittipannu / Dynamo </h2>
+<?php require_once("mysql/average.php");
+getAverage();
+?>
 <p> Todays menu: </p>
 <div class="bitti-today"></div>
 </div>
--- pages/fiilu.php
+++ pages/fiilu.php
@@ -1,2 +1,4 @@
-
+<?php require_once("mysql/average.php");
+getAverage();
+?>
 <div class="food-fiilu"></div>
