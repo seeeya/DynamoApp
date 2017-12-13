89114402be7e83ec0c41a7fbe9dd7dcb4ee25728
Niko Valkonen <K2543@LABRANET.JAMK.FI>
2017-12-13 13:20:53 +0200

    changed some stuff for fiilu :D

--- pages/fiilu.php
+++ pages/fiilu.php
@@ -1,5 +1,10 @@
+
+<div class="page-container"><h2>Restaurant Fiilu</h2>
 <?php
 require_once("mysql/average.php");
 getAverage();
 ?>
+<p> This weeks menu: </p>
 <div class="food-fiilu"></div>
+</div>
+
