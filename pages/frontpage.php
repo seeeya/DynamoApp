e02c51447d0cfa293c76dc31c510bf0d82b72178
seeeya <nikoatte95@gmail.com>
2017-12-14 00:02:36 +0200

    added page container

--- pages/frontpage.php
+++ pages/frontpage.php
@@ -2,7 +2,9 @@
 require_once("functions.php");
 $page = new sitesData();
 ?>
-
+<div class="page-container">
 <h2>Welcome to LunchApp</h2> 
 <h4>You can find content from here:</h4>
 <?=$page->listMenu();?>
+
+</div>
